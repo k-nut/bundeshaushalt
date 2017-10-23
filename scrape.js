298f8a2e96bda5e6beb47f59e31921c036bd5896
Knut Hühne <knut@k-nut.eu>
2017-10-23 20:31:44 +0200

    do not crash on single failure and prettify

--- scrape.js
+++ scrape.js
@@ -1,28 +1,29 @@
-require("isomorphic-fetch");
-const fs = require('fs');
+const fetch = require("isomorphic-fetch");
+const fs = require("fs");
+const pRetry = require("p-retry");
 
 const results = [];
-const baseURL = "https://www.bundeshaushalt-info.de/rest/2017/soll/einnahmen/funktion/";
+const baseURL =
+  "https://www.bundeshaushalt-info.de/rest/2017/soll/einnahmen/funktion/";
 
-const makeRequest = (id) => {
-    const suffix = id ? `${id}/` : '';
-    return fetch(`${baseURL}${suffix}`)
-        .then(r => {
-            console.log(r.url);
-            return r.json();
-        })
-        .then(json => {
-            const leafChildren = json.childs.filter(c => c.titleDetail);
-            results.push(...leafChildren);
-            const branchChildren = json.childs.filter(c => !c.titleDetail);
-            return Promise.all(branchChildren.map(c => c.a).map(makeRequest));
-        }).catch(e => {
-            // TODO: Retry? This also crashes the Promise.all ... 🤔
-            console.log(e);
-        })
+const makeRequest = id => {
+  const suffix = id ? `${id}/` : "";
+  return fetchRetry(`${baseURL}${suffix}`)
+    .then(r => {
+      console.log(r.url);
+      return r.json();
+    })
+    .then(json => {
+      const leafChildren = json.childs.filter(c => c.titleDetail);
+      results.push(...leafChildren);
+      const branchChildren = json.childs.filter(c => !c.titleDetail);
+      return Promise.all(branchChildren.map(c => c.a).map(makeRequest));
+    });
 };
 
+const fetchRetry = url => pRetry(() => fetch(url), { retries: 5 });
+
 makeRequest().then(() => {
-    const json = JSON.stringify(results);
-    fs.writeFile('functions.json', json, 'utf8', () => console.log("Done! 🎉"));
+  const json = JSON.stringify(results);
+  fs.writeFile("functions.json", json, "utf8", () => console.log("Done! 🎉"));
 });
